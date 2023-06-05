1e4d876bea78270001e095cb8b537bb254b991cf
amarieldanendra <aammaarriieell@gmail.com>
2023-06-05 22:27:19 +0700

    fixed

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,4 +1,4 @@
-
+import clientPromise from "@/lib/mongodb";
 export default async function handler(req,res){
     const client = await clientPromise
     const db =  client.db(process.env.MONGODB_DB)
